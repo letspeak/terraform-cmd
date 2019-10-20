1e31b2b79773a7a7b5ba9ac84e5521096da61943
letspeak <whocares@whocares.com>
2019-10-20 17:50:36 -0500

    Terraform keep going

--- resources.tf
+++ resources.tf
@@ -6,3 +6,35 @@ resource "aws_vpc" "environment_example_aws" {
     Name = "terraform_aws_vpc"
   }
 }
+
+resource "aws_subnet" "pub_us_east_2a" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment_example_aws.cidr_block,8, 1)}"
+  vpc_id = "${aws_vpc.environment_example_aws.id}"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "tf_pub_us_e_2a"
+  }
+}
+
+resource "aws_subnet" "pri_us_east_2b" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment_example_aws.cidr_block,8, 2)}"
+  vpc_id = "${aws_vpc.environment_example_aws.id}"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "tf_pri_us_e_2b"
+  }
+}
+
+resource "aws_security_group" "sub_sec_grp" {
+  vpc_id = "${aws_vpc.environment_example_aws.id}"
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment_example_aws.cidr_block}"
+    ]
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  }
+}

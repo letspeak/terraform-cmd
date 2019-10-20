76e6978223594d0174fff88e4ec8246e517a937a
letspeak <whocares@whocares.com>
2019-10-20 17:13:51 -0500

    Terraform Hello World

--- resources.tf
+++ resources.tf
@@ -1,1 +1,8 @@
-resource "vp" "" {}
+resource "aws_vpc" "environment_example_aws" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = {
+    Name = "terraform_aws_vpc"
+  }
+}
